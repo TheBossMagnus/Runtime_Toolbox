ab7e8d9d47e2e549034215ef42f39ef8b00c9d3f
THEBOSSMAGNUS <92474350+THEBOSSMAGNUS@users.noreply.github.com>
2022-04-19 13:30:34 +0200

    code cleanup

--- Runtime toolbox.ps1
+++ Runtime toolbox.ps1
@@ -19,15 +19,13 @@ If ($RUN -ine "Y") {exit}
 Clear-Host
 
 #check if Chocolatey is intalled
-
 Write-Host -f green "Loading..."
-
 if (Test-Path C:\ProgramData\chocolatey\choco.exe)
  {}  
-else{Start-Process PowerShell -WindowStyle Hidden -ArgumentList "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-	
+else{Start-Process PowerShell -WindowStyle Hidden -ArgumentList "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"	
 }
 Start-Sleep -Seconds 30
+
 #runtime install 
 choco install vcredist140 -y | Out-Null
 Write-Host "MSVC 2015-2022 installed" -f green
