8b9eb30f01ca03c691f25c9226d579111f444e5b
THEBOSSMAGNUS <tbmagnusgmaer@gmail.com.com>
2022-05-09 17:27:23 +0200

    Minor code quality(whitespace)

--- Runtime toolbox.ps1
+++ Runtime toolbox.ps1
@@ -1,32 +1,32 @@
 
-#intro 
+#intro
 Set-ExecutionPolicy Unrestricted
 Write-Host -f DarkYellow "
-                                            welcome in 
-______               _    _                    _____              _  _                 
-| ___ \             | |  (_)                  |_   _|            | || |                
+                                            welcome in
+______               _    _                    _____              _  _    
+| ___ \             | |  (_)                  |_   _|            | || |
 | |_/ /_   _  _ __  | |_  _  _ __ ___    ___    | |  ___    ___  | || |__    ___ __  __
 |    /| | | || '_ \ | __|| || '_ ` _ \  / _ \   | | / _ \  / _ \ | || '_ \  / _ \\ \/ /
-| |\ \| |_| || | | || |_ | || | | | | ||  __/   | || (_) || (_) || || |_) || (_) |>  < 
+| |\ \| |_| || | | || |_ | || | | | | ||  __/   | || (_) || (_) || || |_) || (_) |>  <
 \_| \_|\__,_||_| |_| \__||_||_| |_| |_| \___|   \_/ \___/  \___/ |_||_.__/  \___//_/\_\ by THEBOSSMAGNUS
 
 			   This program will install all common used runtime"
- 
+
 
 #chose if run y/N
 $RUN = Read-Host "I do not take any responsibilities for any damage caused by this program. Do you want to run this program? Y=yes N=no"
-If ($RUN -ine "Y") {exit} 
+If ($RUN -ine "Y") {exit}
 Clear-Host
 
 #check if Chocolatey is intalled
 Write-Host -f green "Loading..."
 if (Test-Path C:\ProgramData\chocolatey\choco.exe)
- {}  
-else{Start-Process PowerShell -WindowStyle Hidden -ArgumentList "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"	
+ {}
+else{Start-Process PowerShell -WindowStyle Hidden -ArgumentList "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 }
 Start-Sleep -Seconds 30
 
-#runtime install 
+#runtime install
 choco install vcredist140 -y | Out-Null
 Write-Host "MSVC 2015-2022 installed" -f green
 choco install vcredist2013 -y | Out-Null
@@ -38,7 +38,7 @@ Write-Host "MSVC 2010 installed" -f green
 choco install vcredist2008  -y | Out-Null
 Write-Host "MSVC 2008 installed" -f green
 choco install vcredist2005-y | Out-Null
-Write-Host "MSVC 2005 installed" -f green 
+Write-Host "MSVC 2005 installed" -f green
 choco install javaruntime  -y | Out-Null
 Write-Host "Java 8 runtime envirorment installed" -f green
 choco install xna  -y | Out-Null
@@ -52,7 +52,7 @@ Write-Host "Silverlight installed" -f green
 choco install directx | Out-Null
 Write-Host "Directx 9" -f green
 
-#done/reboot 
+#done/reboot
 $REBOOT = Read-Host "Everything is installed, a reboot is suggested. Do you want to reboot now? Y=yes N=no"
 If ($REBOOT -ieq "Y") {restart-computer}
 else {exit}
