a78a2f97a0a8a80bf5c028d0ea84f59104f60175
THEBOSSMAGNUS <tbmagnusgamer@gmail.com>
2022-05-20 17:48:54 +0200

    TestV1

--- Runtime ToolboxGUI WIP.ps1
+++ Runtime ToolboxGUI WIP.ps1
@@ -1,19 +1,37 @@
-function GenerateForm {
+
+<#
+	.SYNOPSIS
+	Version: WIP
+	Date: 19.05.2022
+	Copyright (c) 2022 THEBOSSMAGNUS
+	.DESCRIPTION
+	A simple and open source tool for install runtime.
+	.NOTES
+	Set execution policy if it fail to run:
+		Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+	.NOTES
+	Require admin execution
+    .NOTES
+    For windows 10 & 11. Require Powershell 5(preinstalled in windows 10/11) or 7
+	.LINK
+	https://github.com/THEBOSSMAGNUS/Runtime_Toolbox
+#>
+
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 
-# Gui
+#Gui
 #main form
-    $Form1 = New-Object system.Windows.Forms.Form
-        $Form1.text = "Runtime Toolbox"
-        $Form1.ClientSize = '527, 300'
-        $Form1.FormBorderStyle = 'FixedSingle'
+    $Windows = New-Object system.Windows.Forms.Form
+        $Windows.text = "Runtime Toolbox"
+        $Windows.ClientSize = '527, 300'
+        $Windows.FormBorderStyle = 'FixedSingle'
 
 #install button
     $button = New-Object System.Windows.Forms.Button
-        $form1.Controls.Add($button)
+        $Windows.Controls.Add($button)
         $button.TabIndex = 4
         $button.Width = 250
         $button.Height = 50
@@ -23,28 +41,26 @@ Add-Type -AssemblyName System.Windows.Forms
         $button.add_Click($handler_button_Click)
 
 #Stust console
-    $listBox1 = New-Object System.Windows.Forms.ListBox
-        $form1.Controls.Add($listBox1)
-        $listBox1.Width = 250
-        $listBox1.Height = 150
-        $listBox1.Location = New-Object System.Drawing.Point(250,13)
-        $listBox1.TabIndex = 3
+    $Console = New-Object System.Windows.Forms.ListBox
+        $Windows.Controls.Add($Console)
+        $Console.Width = 250
+        $Console.Height = 150
+        $Console.Location = New-Object System.Drawing.Point(250,13)
+        $Console.TabIndex = 3
 
 
 #checkbox
     #MSVC 2015-2022
     $VC1522 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($VC1522)
+        $Windows.Controls.Add($VC1522)
         $VC1522.Width = 105
         $VC1522.Height = 30
         $VC1522.location = New-Object System.Drawing.Point(27,10)
         $VC1522.Text = "C++ visual 2015-2022"
 
-
-
  #MSVC 2013
     $VC13 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($VC13)
+        $Windows.Controls.Add($VC13)
         $VC13.Width = 105
         $VC13.Height = 30
         $VC13.location = New-Object System.Drawing.Point(27,50)
@@ -52,7 +68,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
  #MSVC 2012
      $VC12 = New-Object System.Windows.Forms.CheckBox
-         $form1.Controls.Add($VC12)
+         $Windows.Controls.Add($VC12)
          $VC12.Width = 105
          $VC12.Height = 30
          $VC12.location = New-Object System.Drawing.Point(27,90)
@@ -60,7 +76,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
  #MSVC 2010
     $VC10 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($VC10)
+        $Windows.Controls.Add($VC10)
         $VC10.Width = 105
         $VC10.Height = 30
         $VC10.location = New-Object System.Drawing.Point(27,130)
@@ -68,7 +84,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
 #MSVC 2008
     $VC8 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($VC8)
+        $Windows.Controls.Add($VC8)
         $VC8.Width = 105
         $VC8.Height = 30
         $VC8.location = New-Object System.Drawing.Point(27,170)
@@ -76,7 +92,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
 #MSVC 2005
     $VC5 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($VC5)
+        $Windows.Controls.Add($VC5)
         $VC5.Width = 105
         $VC5.Height = 30
         $VC5.location = New-Object System.Drawing.Point(27,210)
@@ -84,8 +100,8 @@ Add-Type -AssemblyName System.Windows.Forms
 
 #Java
     $Java = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($Java)
-        $form1.Controls.Add($Java)
+        $Windows.Controls.Add($Java)
+        $Windows.Controls.Add($Java)
         $Java.Width = 105
         $Java.Height = 30
         $Java.location = New-Object System.Drawing.Point(145,10)
@@ -93,7 +109,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
  #.net 4.8
     $NET48 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($NET48)
+        $Windows.Controls.Add($NET48)
         $NET48.Width = 105
         $NET48.Height = 30
         $NET48.location = New-Object System.Drawing.Point(145,50)
@@ -101,23 +117,23 @@ Add-Type -AssemblyName System.Windows.Forms
 
 #.net 3.5
     $NET35 = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($NET35)
+        $Windows.Controls.Add($NET35)
         $NET35.Width = 105
         $NET35.Height = 30
         $NET35.location = New-Object System.Drawing.Point(145,90)
         $NET35.Text = ".NET Framework 3.5"
 
 #DirectX 9
-    $DX = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($DX)
-        $DX.Width = 105
-        $DX.Height = 30
-        $DX.location = New-Object System.Drawing.Point(145,130)
-        $DX.Text = "Directx 9"
+    $DX9 = New-Object System.Windows.Forms.CheckBox
+        $Windows.Controls.Add($DX9)
+        $DX9.Width = 105
+        $DX9.Height = 30
+        $DX9.location = New-Object System.Drawing.Point(145,130)
+        $DX9.Text = "Directx 9"
 
 #XNA
     $XNA = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($XNA)
+        $Windows.Controls.Add($XNA)
         $XNA.Width = 105
         $XNA.Height = 30
         $XNA.location = New-Object System.Drawing.Point(145,170)
@@ -125,51 +141,89 @@ Add-Type -AssemblyName System.Windows.Forms
 
 #Silverlight
     $SL = New-Object System.Windows.Forms.CheckBox
-        $form1.Controls.Add($SL)
+        $Windows.Controls.Add($SL)
         $SL.Width = 105
         $SL.Height = 30
         $SL.location = New-Object System.Drawing.Point(145,210)
-        $SL.Text = "Sileverlight"
+        $SL.Text = "Silverlight"
 
+$Windows.ShowDialog() #Show the Form
 
-
-
-
-
-$form1.ShowDialog() #Show the Form
-}
-
-
-function Msvc20152022 {
-    choco install vcredist140 -y | Out-Null
-    Write-Host "MSVC 2015-2022 installed" -f green
-    $listBox1.Items.Add("MSVC 2015-2022 installed")}
-
-function Java {
-    choco install javaruntime  -y | Out-Null
-    Write-Host "Java 8 runtime envirorment installed" -f green
-    $listBox1.Items.Add("Java 8 runtime envirorment installed")}
-
-function netfx48 {
-    choco install netfx-4.8  -y | Out-Null
-    Write-Host "Lorem Ipsum installed" -f green
-    $listBox1.Items.Add("Lorem Ipsum installed")}
-
-
-
-
-#Handler
 $handler_button_Click= {
-    $listBox1.Items.Clear();
+    $console.Items.Clear();
 
     if ($MSVC1522.Checked)    {Msvc20152022}
 
+    if ($MSVC13.Checked)    {Msvc2013}
+
+    if ($MSVC12.Checked)    {Msvc2012}
+
+    if ($MSVC10.Checked)    {Msvc2010}
+
+    if ($MSVC8.Checked)    {Msvc20152022}
+
+    if ($MSVC5.Checked)    {Msvc2005}
+
     if ($Java.Checked)    {Java}
 
-    if ($WIP.Checked)    {netfx48}
+    if ($NET48.Checked)    {netfx48}
 
-    if (!$MSVC1522.Checked -and !$Java.Checked -and !$WIP.Checked) {$listBox1.Items.Add("No runtime selected....")}}
+    if ($NET35.Checked)    {netfx35}
 
+    if ($DX9.Checked)    {Directx9}
 
-#Call the Function
-GenerateForm
+    if ($XNA.Checked)    {XNA}
+
+    if ($SL.Checked)    {Silverlight}
+
+    $console.Items.Add("All selected runtimes have been installed")
+
+    if (!$MSVC1522.Checked -and !$Java.Checked -and !$WIP.Checked) {$console.Items.Add("No runtime selected....")}}
+
+function Msvc20152022 {
+    choco install vcredist140 -y
+    $Console.Items.Add("MSVC 2015-2022 installed")}
+
+function Msvc2013 {
+    choco install vcredist2013 -y
+    $Console.Items.Add("MSVC 2013 installed")}
+
+function Msvc2012 {
+    choco install vcredist2012
+    $Console.Items.Add("MSVC 2012 installed")}
+
+function Msvc2010 {
+    choco install vcredist2010 -y
+    $Console.Items.Add("MSVC 2010 installed")}
+
+function Msvc2008 {
+    choco install vcredist2008  -y
+    $Console.Items.Add("MSVC 2008 installed")}
+
+function Msvc2005 {
+    choco install vcredist2005-y
+    $Console.Items.Add("MSVC 2005 installed")}
+
+function Java {
+    choco install javaruntime  -y
+    $Console.Items.Add("Java 8 runtime envirorment installed")}
+
+function netfx48 {
+    Enable-WindowsOptionalFeature -Online -FeatureName "NetFx4-AdvSrvs" -All
+    $Console.Items.Add("Net Framework 4.8 installed")}
+
+function netfx35 {
+    Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All
+    $Console.Items.Add("Net Framework 3.5 installed")}
+
+function Directx9 {
+    choco install directx -y
+    $Console.Items.Add("Net Framework 3.5 installed")}
+
+function XNA {
+    choco install xna  -y
+    $Console.Items.Add("Xna Framework installed")}
+
+function Silverlight {
+    choco install silverlight -y
+    $Console.Items.Add("Silverlight installed")}
